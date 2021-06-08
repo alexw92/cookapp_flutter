30bf1e8c9d5b05ceade8a5e5454a869ccc4a4ab6
Alexander Werthmann <werdy@gmx.net>
2021-06-08 12:56:48 +0200

    added models, deserialization

---
+++ lib/core/data/models.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+class Recipe {
+  final int id;
+  final String img_src;
+  final String name;
+  final String uploadedBy;
+  final List<RecipeInstruction> instructions;
+  final List<Ingredient> ingredients;
+
+  Recipe(
+      {this.id,
+      this.img_src,
+      this.name,
+      this.uploadedBy,
+      this.instructions,
+      this.ingredients});
+
+  factory Recipe.fromJson(Map<String, dynamic> recipeJson) {
+    return Recipe(
+        id: recipeJson['id'],
+        img_src: recipeJson['img_src'],
+        name: recipeJson['name'],
+        uploadedBy: recipeJson['uploadedBy'],
+        instructions: (recipeJson['instructions'] as List)
+            .map((it) => RecipeInstruction.fromJson(it))
+            .toList(),
+        ingredients: (recipeJson['ingredients'] as List)
+            .map((it) => Ingredient.fromJson(it))
+            .toList());
+  }
+}
+
+class RecipeInstruction {
+  final int id;
+  final int recipeId;
+  final int step;
+  final String instructionsText;
+
+  RecipeInstruction({this.id, this.recipeId, this.step, this.instructionsText});
+
+  factory RecipeInstruction.fromJson(Map<String, dynamic> json) {
+    return RecipeInstruction(
+        id: json['id'],
+        recipeId: json['recipeId'],
+        step: json['step'],
+        instructionsText: json['instructionsText']);
+  }
+}
+
+class Ingredient {
+  final int id;
+  final String name;
+  final int amount;
+  final int recipeId;
+  final int foodProductId;
+
+  Ingredient({
+    this.id,
+    this.name,
+    this.amount,
+    this.recipeId,
+    this.foodProductId,
+  });
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+        id: json['id'],
+        name: json['name'],
+        amount: json['amount'],
+        recipeId: json['recipeId'],
+        foodProductId: json['foodProductId']);
+  }
+}
--- lib/core/io/controllers.dart
+++ lib/core/io/controllers.dart
@@ -1,19 +1,22 @@
 import 'dart:io';
+import 'dart:convert';
+import 'package:cookable_flutter/core/data/models.dart';
 import 'package:http/http.dart' as http;
 
 class RecipeController {
-  static Future<http.Response> getRecipes() async {
+  static Future<List<Recipe>> getRecipes() async {
     var response =
         await http.get(Uri.parse("http://192.168.2.102:8080/recipes"));
 
     /// If the first API call is successful
     if (response.statusCode == HttpStatus.ok) {
-      print(response);
+      //  print(response);
     }
 
-    /// Do other stuffs here
     print("lol");
-    print(response.body);
-    return response;
+    var list = json.decode(response.body) as List;
+    List<Recipe> recipes = list.map((it) => Recipe.fromJson(it)).toList();
+    print("List contains ${list.length} recipes.");
+    return recipes;
   }
 }
