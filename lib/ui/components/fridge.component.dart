216d2582c438d921239f166fdf452470a055f97e
Janki Vaidya <jankivaidya@Jankis-MacBook-Air.local>
2021-06-12 22:48:53 +0200

    Add the normal grid

--- lib/ui/components/fridge.component.dart
+++ lib/ui/components/fridge.component.dart
@@ -31,27 +31,55 @@ class _FridgeComponentState extends State<FridgeComponent> {
     ingredientsList.add(baguette);
     ingredientsList.add(avocado);
     ingredientsList.add(avocado);
+    ingredientsList.add(baguette);
     ingredientsList.add(avocado);
-    ingredientsList.add(avocado);
+    ingredientsList.add(baguette);
+
+    // return Container(
+    //   color: Colors.black,
+    //   child: new StaggeredGridView.countBuilder(
+    //     shrinkWrap: true,
+    //     crossAxisCount: 3,
+    //     itemCount: ingredientsList.length,
+    //     itemBuilder: (BuildContext context, int index) => new Container(
+    //       child: new Center(
+    //         child: FridgeTileComponent(
+    //           ingredient: ingredientsList[index],
+    //         ),
+    //       ),
+    //     ),
+    //     staggeredTileBuilder: (int index) => new StaggeredTile.extent(
+    //         2, (1 / 3) * MediaQuery.of(context).size.width),
+    //     mainAxisSpacing: 4.0,
+    //     crossAxisSpacing: 4.0,
+    //   ),
 
     return Container(
       color: Colors.black,
-      child: new StaggeredGridView.countBuilder(
-        shrinkWrap: true,
-        crossAxisCount: 3,
-        itemCount: ingredientsList.length,
-        itemBuilder: (BuildContext context, int index) => new Container(
-          child: new Center(
-            child: FridgeTileComponent(
-              ingredient: ingredientsList[index],
-            ),
-          ),
+      child: Container(
+        height: 400,
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: GridView.count(
+          primary: true,
+          padding: const EdgeInsets.all(0),
+          crossAxisCount: 3,
+          mainAxisSpacing: 0,
+          crossAxisSpacing: 0,
+          children: [...getAllTiles()],
         ),
-        staggeredTileBuilder: (int index) => new StaggeredTile.extent(
-            2, (1 / 3) * MediaQuery.of(context).size.width),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
       ),
     );
+  }
+
+  List<Widget> getAllTiles() {
+    List<Widget> myTiles = [];
+    for (int i = 0; i < ingredientsList.length; i++) {
+      myTiles.add(
+        FridgeTileComponent(
+          ingredient: ingredientsList[i],
+        ),
+      );
+    }
+    return myTiles;
   }
 }
