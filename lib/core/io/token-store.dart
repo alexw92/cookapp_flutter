56f0db74a11acebde5ae015b02d92364cd631262
Alexander Werthmann <werdy@gmx.net>
2021-06-14 18:28:42 +0200

    removed unused function

--- lib/core/io/token-store.dart
+++ lib/core/io/token-store.dart
@@ -4,6 +4,7 @@ import 'package:jwt_decoder/jwt_decoder.dart';
 
 class TokenStore {
   final storage = new FlutterSecureStorage();
+
   TokenStore._privateConstructor();
 
   static final TokenStore _instance = TokenStore._privateConstructor();
@@ -12,31 +13,26 @@ class TokenStore {
     return _instance;
   }
 
-  putToken(String userId, String token){
+  putToken(String userId, String token) {
     return storage.write(key: userId, value: token);
   }
 
-  getToken() async{
+  getToken() async {
     var user = FirebaseAuth.instance.currentUser;
     var storedToken = await storage.read(key: user.uid);
 
-    if(storedToken==null || JwtDecoder.isExpired(storedToken) || JwtDecoder.getRemainingTime(storedToken).inSeconds < 10){
+    if (storedToken == null ||
+        JwtDecoder.isExpired(storedToken) ||
+        JwtDecoder.getRemainingTime(storedToken).inSeconds < 10) {
       String token = await user.getIdToken(true);
       putToken(user.uid, token);
       return token;
-    }
-    else {
+    } else {
       return storedToken;
     }
   }
 
-  deleteToken(String userId){
+  deleteToken(String userId) {
     return storage.delete(key: userId);
   }
-
-
-  _isExpiredToken(String token){
-
-  }
-
 }
