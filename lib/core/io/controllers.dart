bb95b4b82b4617bf781feb28503d847343198109
Alexander Werthmann <werdy@gmx.net>
2021-06-14 17:41:26 +0200

    refactoring, added todo

--- lib/core/io/controllers.dart
+++ lib/core/io/controllers.dart
@@ -6,20 +6,23 @@ import 'package:cookable_flutter/core/io/token-store.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 
+// todo: logic to get fresh fb token should be part of token store
+
 class RecipeController {
+  static final tokenStore = TokenStore();
 
   static Future<List<Recipe>> getRecipes() async {
-    // todo this is just for testing, needs to be refactored
     // get token from firebase
-    final tokenStore = TokenStore();
+
     var user = FirebaseAuth.instance.currentUser;
     String token = await user.getIdToken();
     // save token in token store
     tokenStore.putToken(user.uid, token);
     // get token from token store
     String storedToken = await tokenStore.getToken(user.uid);
-    var response =
-        await http.get(Uri.parse("http://192.168.2.102:8080/recipes"),headers: {"Authorization": "Bearer $storedToken"});
+    var response = await http.get(
+        Uri.parse("http://192.168.2.102:8080/recipes"),
+        headers: {"Authorization": "Bearer $storedToken"});
 
     /// If the first API call is successful
     if (response.statusCode == HttpStatus.ok) {
@@ -28,12 +31,22 @@ class RecipeController {
       return recipes;
     }
 
-    throw Exception("Error retrieving recipes, Code: ${response.statusCode} Message: ${response.body} ");
+    throw Exception(
+        "Error retrieving recipes, Code: ${response.statusCode} Message: ${response.body} ");
   }
 }
 
 class FoodProductController {
+  static final tokenStore = TokenStore();
+
   static Future<List<FoodProduct>> getFoodProducts() async {
+    var user = FirebaseAuth.instance.currentUser;
+    String token = await user.getIdToken();
+    // save token in token store
+    tokenStore.putToken(user.uid, token);
+    // get token from token store
+    String storedToken = await tokenStore.getToken(user.uid);
+
     var response =
         await http.get(Uri.parse("http://192.168.2.102:8080/foodProducts"));
 
@@ -41,10 +54,11 @@ class FoodProductController {
     if (response.statusCode == HttpStatus.ok) {
       var list = json.decode(response.body) as List;
       List<FoodProduct> foodProducts =
-      list.map((it) => FoodProduct.fromJson(it)).toList();
+          list.map((it) => FoodProduct.fromJson(it)).toList();
       return foodProducts;
     }
 
-    throw Exception("Error retrieving foodProducts, Code: ${response.statusCode} Message: ${response.body} ");
+    throw Exception(
+        "Error retrieving foodProducts, Code: ${response.statusCode} Message: ${response.body} ");
   }
 }
