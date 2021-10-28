2e665a45490130034fce20d9722a75060c38ef21
alexw92 <werdy@gmx.net>
2021-10-29 00:20:01 +0200

    wraped into stack

--- lib/ui/components/recipe-tile.component.dart
+++ lib/ui/components/recipe-tile.component.dart
@@ -22,58 +22,67 @@ class _RecipeTileComponentState extends State<RecipeTileComponent> {
 
   _RecipeTileComponentState({this.recipe, this.apiToken});
 
-  // Todo 1: Fix overflow shit and keep it kinda responsive
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () => navigateToRecipePage(recipe.id),
-        child: Card(
-            //padding: new EdgeInsets.all(20.0),
-            child:
-          Container(
-              height: 300,
-              width: 300,
-              child: FittedBox(fit: BoxFit.fill,
+        child: Container(
+          alignment: Alignment.center,
+
+          child: Stack(children: [
+            Container(
+                height: 300,
+                width: 300,
+                child: FittedBox(
+                  fit: BoxFit.fill,
                   child: Image(
-            // needs --web-renderer html
-            image:
-                CachedNetworkImageProvider(recipe.imgSrc,
-                    imageRenderMethodForWeb: ImageRenderMethodForWeb.HttpGet),
-            // backgroundColor: Colors.transparent,
-            //  radius: 40,
-          ))),
-          // Container(
-          //     alignment: Alignment.center,
-          //     child: Text(
-          //       recipe.name,
-          //       style: CookableTheme.normalBlackFont,
-          //     )
-         // ),
+                    // needs --web-renderer html
+                    image: CachedNetworkImageProvider(recipe.imgSrc,
+                        imageRenderMethodForWeb:
+                            ImageRenderMethodForWeb.HttpGet),
+                    // backgroundColor: Colors.transparent,
+                    //  radius: 40,
+                  ),
+                )),
+            Container(
+                width: 100,
+                height: 50,
+                color: Color.fromARGB(150, 255, 255, 255),
+                child: Text("Laaaaawwlllll")),
+            Container(
+              width: 30,
+              height: 30,
+              color: Color.fromARGB(200, 200, 0, 0),
+            )
+          ]),
+        )
+        // Container(
+        //     alignment: Alignment.center,
+        //     child: Text(
+        //       recipe.name,
+        //       style: CookableTheme.normalBlackFont,
+        //     )
+        // ),
 
-          // removed amount from api
-          // Text(
-          //   "${Utility.getFormattedAmount(userFoodProduct)}",
-          //   style: CookableTheme.smallWhiteFont,
-          // ),
+        // removed amount from api
+        // Text(
+        //   "${Utility.getFormattedAmount(userFoodProduct)}",
+        //   style: CookableTheme.smallWhiteFont,
+        // ),
 
-      ),
-      // Todo this is where I want to add #persons, diet icon, nutrients
-      // Container(
-      //   height: 50,
-      //   alignment: Alignment.bottomCenter,
-      //   color: Colors.orange,
-      // ),
-    );
+        // Todo this is where I want to add #persons, diet icon, nutrients
+        // Container(
+        //   height: 50,
+        //   alignment: Alignment.bottomCenter,
+        //   color: Colors.orange,
+        // ),
+        );
   }
 
   navigateToRecipePage(int recipeId) async {
     //navigate to materials page when callbackPath is not report page and list component is clicked
     //Return the result to this component when task is finished from materials
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) =>
-              RecipesDetailsPage(recipeId))
-    );
+    final result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => RecipesDetailsPage(recipeId)));
   }
 }
