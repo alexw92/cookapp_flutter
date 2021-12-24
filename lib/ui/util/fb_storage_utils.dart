67e710eb3061bf188d59bbc5332d66274115d293
alexw92 <werdy@gmx.net>
2021-12-24 23:25:19 +0100

    added forgotten file lol

---
+++ lib/ui/util/fb_storage_utils.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
+
+class FBStorage{
+
+
+  static Future<String> getPrivateRecipeImgDownloadUrl(String imgRef) async {
+    String downloadURL = await firebase_storage.FirebaseStorage.instance
+       // .ref('user/1Oh3tB7kM6hKfPc3ptsE2S0N7Ov2/recipeImgs/0714f324-e98c-444c-9be9-b7d5574ec97d.jpg')
+        .ref(imgRef)
+        .getDownloadURL();
+    return downloadURL;
+  }
+
+
+
+}
