c63417752c315c7258b47952d6e7902b13d13d9f
alexw92 <werdy@gmx.net>
2021-12-27 20:23:39 +0100

    added hive

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hive/hive.dart';
+import "package:hive_flutter/hive_flutter.dart";
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'common/LangState.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   await Firebase.initializeApp();
   runApp(CookableFlutter());
 }
