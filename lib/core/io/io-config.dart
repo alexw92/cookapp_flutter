c67d09874d7bbd880b2337be0e2fcae993ef6b23
Alexander Werthmann <werdy@gmx.net>
2022-01-16 18:59:21 +0100

    new server online :)

--- lib/core/io/io-config.dart
+++ lib/core/io/io-config.dart
@@ -5,6 +5,6 @@ class IOConfig{
   static final timeoutDuration = Duration(seconds: 15);
   static final tokenStore = TokenStore();
   static final apiUrlDev = "http://192.168.2.102:8080";
-  static final apiUrlLive = "http://cookable.eu-central-1.elasticbeanstalk.com";
-  static final apiUrl = apiUrlDev;
+  static final apiUrlLive = "http://alexanderwerthmann.de:8080";
+  static final apiUrl = apiUrlLive;
 }
