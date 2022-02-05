cbcf0bf4dcc6de6d4a29fce4193fbd7f60b3a525
Alexander Werthmann <werdy@gmx.net>
2022-02-05 23:08:22 +0100

    api is now on https

--- lib/core/io/io-config.dart
+++ lib/core/io/io-config.dart
@@ -5,6 +5,6 @@ class IOConfig{
   static final timeoutDuration = Duration(seconds: 15);
   static final tokenStore = TokenStore();
   static final apiUrlDev = "http://192.168.2.102:8080";
-  static final apiUrlLive = "http://alexanderwerthmann.de:8080";
+  static final apiUrlLive = "https://alexanderwerthmann.de:8443";
   static final apiUrl = apiUrlLive;
 }
